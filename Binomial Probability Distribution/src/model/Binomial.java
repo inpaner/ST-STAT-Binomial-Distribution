b8c243590f6c6ac552b4aa641cff5587f32a874b
Ivan Paner <ivan.paner@gmail.com>
2013-08-24 23:06:06 +0800

    Added Binomial field values

--- Binomial Probability Distribution/src/model/Binomial.java
+++ Binomial Probability Distribution/src/model/Binomial.java
@@ -5,13 +5,20 @@ import org.apache.commons.math3.util.ArithmeticUtils;
 
 public class Binomial {
     private double[] probabilities;    
+    private int start;
+    private int end;
+    private int n;
+    private double p;
     
     public Binomial(int x, int n, double p) {
-        double[] probabilities = new double[1];
-        probabilities[0] = solveProbability(x, n, p);
+        this(x, x, n, p);
     }
     
     public Binomial(int xStart, int xEnd, int n, double p) {
+        start = xStart;
+        end = xEnd;
+        this.n = n;
+        this.p = p;
         double[] probabilities = new double[xEnd - xStart+1];
         int index = 0;
         
@@ -43,5 +50,22 @@ public class Binomial {
         return ArithmeticUtils.factorial(n) /
                 (ArithmeticUtils.factorial(r) * ArithmeticUtils.factorial(n - r));
     }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public double getP() {
+        return p;
+    }
+    
 }
 
