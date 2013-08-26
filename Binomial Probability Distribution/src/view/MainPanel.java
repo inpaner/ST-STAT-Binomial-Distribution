ce419a80a823c1f2174fd804ee82995bb6c7ed8b
Ivan Paner <ivan.paner@gmail.com>
2013-08-26 17:47:49 +0800

    Added table output

--- Binomial Probability Distribution/src/view/MainPanel.java
+++ Binomial Probability Distribution/src/view/MainPanel.java
@@ -3,10 +3,13 @@ package view;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import model.Binomial;
 import net.miginfocom.swing.MigLayout;
 
 public class MainPanel extends JPanel {
     private InputPanel inputPanel;
+    private JTabbedPane tabbedPane;
+    private OutputTable outputTable;
     
     public MainPanel() {
         setLayout(new MigLayout());
@@ -16,10 +19,23 @@ public class MainPanel extends JPanel {
     
     private void initComponents() {
         inputPanel = new InputPanel();
-        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane = new JTabbedPane();
+        outputTable = new OutputTable();
+        tabbedPane.addTab("Table", outputTable);
+        Binomial binomial = new Binomial(3, 20, 20, 0.3);
+        outputTable.updateData(binomial);
     }
     
     private void addComponents() {
-        add();
+        add(inputPanel, "wrap");
+        add(tabbedPane, "span, split");
+    }
+    
+    public InputPanel getInputPanel() {
+        return inputPanel;
+    }
+    
+    public OutputTable getOutputTable() {
+        return outputTable;
     }
 }
